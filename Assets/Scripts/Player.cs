495987f88dfd3f81bd636533e0b7d6498e0ed575
RohithKadambari <krohithsarma@gmail.com>
2025-02-20 15:39:29 +0530

    Updated Player Controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     int keys = 0;
+    private Rigidbody PlayerRB;
+    [SerializeField] int speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovements();
     }
+
+    public void PlayerMovements()
+    {
+        var Horizontal=Input.GetAxisRaw("Horizontal");
+        var Vertical=Input.GetAxisRaw("Vertical");
+        PlayerRB.velocity = new Vector3(Horizontal * speed , PlayerRB.velocity.y , Vertical * speed);
+      
+    }
+
+
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("key")){
             keys++;
+            
             Destroy(other.gameObject);
             if(keys>=5){
                 Debug.Log("All 5 Keys are Collected");
