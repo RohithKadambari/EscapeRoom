b8da4b334e1dfe76b6b5ca1d4ab7d70dfd86f5e7
RohithKadambari <krohithsarma@gmail.com>
2025-02-27 08:28:50 +0530

    Update Stamina.cs

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Stamina : MonoBehaviour
 {
+    float maxStamina = 100f;
+    float currentStamina;
+    float StaminaRechargeRate;
+    float sprintDrain;
+
+    KeyCode sprintkey = KeyCode.LeftShift;
+
+    bool isSprinting;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
