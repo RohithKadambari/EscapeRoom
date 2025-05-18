c8471ceebc235257190bdc40f63c817897bea959
Vinnu38 <vkanth717@gmail.com>
2025-05-18 22:10:20 +0530

    Updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -79,26 +79,20 @@ public class Player : MonoBehaviour
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
-
-
             Vector3 moveDirection = transform.forward * vertical + transform.right * horizontal;
             moveDirection.Normalize();
             moveDirection.y = 0;
 
-
             playerRB.velocity = moveDirection * moveSpeed;
-            if (moveDirection.magnitude >= 0.1f)
+            if (moveDirection.magnitude >= 0f)
             {
-
                 Walkbounce();
             }
-
         }
         else
         {
             CanyouStand = false;
         }
-
     }
 
     void MouseControl()
@@ -114,6 +108,7 @@ public class Player : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
+
     public void Walkbounce()
     {
         float bounce = Mathf.Sin(Time.time * bounceFrequency) * bounceAmplitude;
@@ -149,15 +144,14 @@ public class Player : MonoBehaviour
         
         if (other.gameObject.CompareTag("Battery"))
         {
-
             string name = other.gameObject.tag;
             var batteryItem = InventoryManager.Instance.inventoryItems.Find(p => p.itemName == "Battery");
+
             if ((batteryItem == null) || (batteryItem.itemQuantity < InventoryManager.Instance.GetMaxCapcityFor(name)))
             {
                 Debug.Log("Got the battery");
                 InventoryManager.Instance.AddItemsInInventory(name, 1);
                 Destroy(other.gameObject);
-
             }
             else
             {
@@ -168,25 +162,22 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject != null)
+        if (other.gameObject.CompareTag("Door"))
         {
-            if (other.gameObject.CompareTag("Door"))
-            {
-                door = true;
+            door = true;
 
-                if (Input.GetKey(KeyCode.E) && DoorClosed)
-                {
-                    Debug.Log("player");
-                    Door.GetComponent<Animation>().Play("DoorOpening");
-                    DoorClosed = false;
-                }
-                else if (Input.GetKey(KeyCode.E) && !DoorClosed)
-                {
-                    Door.GetComponent<Animation>().Play("DoorClosing");
-                    DoorClosed = true;
-                }
+            if (Input.GetKey(KeyCode.E) && DoorClosed)
+            {
+                Debug.Log("player");
+                Door.GetComponent<Animation>().Play("DoorOpening");
+                DoorClosed = false;
             }
-        }
+            else if (Input.GetKey(KeyCode.E) && !DoorClosed)
+            {
+                Door.GetComponent<Animation>().Play("DoorClosing");
+                DoorClosed = true;
+            }
+            }
 
         if (other.gameObject.CompareTag("Key"))
         {
